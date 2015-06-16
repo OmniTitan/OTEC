834224958514b831947f79df875d2da511fb483f
OmniTitan <miner.shane@gmail.com>
2015-06-16 20:19:16 +1000

    Removed unused functions in Main. Modified onCreatureSpawn to check if in a plotworld, check if too many entitites in plot as specified by config, if so dissallow creature spawn. One bug is that the plugin still measures all entities in the plot and includes those such as carts/boats/orbs etc, need to fix this.

--- src/com/omnititan/shaan/OTEggControl/Main.java
+++ src/com/omnititan/shaan/OTEggControl/Main.java
@@ -1,28 +1,18 @@
 package com.omnititan.shaan.OTEggControl;
 
-import com.worldcretornica.plotme_core.Plot;
-import com.worldcretornica.plotme_core.PlotMapInfo;
 import com.worldcretornica.plotme_core.PlotMeCoreManager;
-import com.worldcretornica.plotme_core.PlotMe_Core;
-import com.worldcretornica.plotme_core.api.IItemStack;
 import com.worldcretornica.plotme_core.api.ILocation;
-import com.worldcretornica.plotme_core.api.IPlayer;
 import com.worldcretornica.plotme_core.api.IWorld;
-import com.worldcretornica.plotme_core.bukkit.api.BukkitPlayer;
+import com.worldcretornica.plotme_core.bukkit.api.BukkitLocation;
 import com.worldcretornica.plotme_core.bukkit.api.BukkitWorld;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.player.PlayerEggThrowEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 
 /**
@@ -46,51 +36,44 @@ public class Main extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent e){
-        Player player = e.getPlayer();
-        player.sendMessage("hello from join");
-        List<OTPlayer> players = new ArrayList<OTPlayer>();
-        OTPlayer p = new OTPlayer(e.getPlayer());
-        players.add(p);
-    }
-    @EventHandler
-    public void onPlayerThrow(PlayerEggThrowEvent c){
-
-        OTPlayer p = new OTPlayer(c.getPlayer());
-        p.setDate();
-
-        c.getPlayer().sendMessage("Hello from Event");
-        p.getDateDiff();
-    }
-
-    @EventHandler
-    public void onInteract(PlayerInteractEvent e){
-        //TODO Add in check for who spawns egg so can send them a message.
-        IPlayer p = new BukkitPlayer(e.getPlayer());
-        //TODO IF Statements to check null status of ifInPlot and ifInWorld
-        String worldPlot = "worlds." + e.getPlayer().getWorld().getName() + ".size";
-        int plotSize = getConfig().getInt(worldPlot);
-        String plotId = PlotMeCoreManager.getInstance().getPlotId(p);
-        e.getPlayer().sendMessage(String.valueOf(plotSize));
-        e.getPlayer().sendMessage(String.valueOf(PlotMeCoreManager.getInstance().getPlotBottomLoc(p.getWorld(), plotId).getX()));
-//        PlotMapInfo map = PlotMeCoreManager.getInstance().getMap((IWorld) e.getPlayer().getWorld());
-
-
-    }
-    @EventHandler
     public void onCreatureSpawn(CreatureSpawnEvent e){
-        //TODO Make this only work in Plotworld
-        //TODO Seperate into its own class for readability and expandability
-        double x = getConfig().getDouble("StopEggSpawnArea.x");
-        double y = getConfig().getDouble("StopEggSpawnArea.y");
-        double z = getConfig().getDouble("StopEggSpawnArea.z");
-        if (e.getEntity().getNearbyEntities(x, y, z).size() > getConfig().getInt("StopEggSpawnAmount")){
-            e.setCancelled(true);
-            return;
+        //Create plotme IWorld instances for world check
+        IWorld world = new BukkitWorld(e.getEntity().getWorld());
+        //If in a plotworld then do eggcontrol
+        if (PlotMeCoreManager.getInstance().isPlotWorld(world) && e.getSpawnReason().name().equals("SPAWNER_EGG")) {
+            //Save location to Plotme usable location class
+            ILocation loc = new BukkitLocation(e.getLocation());
+            //Get the plots ID for later use
+            String plotID = PlotMeCoreManager.getInstance().getPlotId(loc);
+            Bukkit.getServer().broadcastMessage(plotID);
+            //Check if there is actually a plot at this location, getPlotId returns an empty string if nothing is found.
+            if (plotID.equals("")) {
+                e.setCancelled(true);
+                return;
+            }
+
+            //Set loc equal to plot location
+            loc = PlotMeCoreManager.getInstance().getPlotBottomLoc(world, plotID);
+            // Get Plot Size from the config
+            int configSize = getConfig().getInt("worlds." + world.getName() + ".size");
+            // Create modifier, which is the halfway point of x and z of the plot
+            double modifier = (double) (configSize / 2 + 1);
+            //Calculate centre point of the plot for entity spawn location variable
+            Location centrePoint = new Location(e.getEntity().getWorld(), loc.getX() + modifier, loc.getY(), loc.getZ() + modifier);
+            EntityType orb = EntityType.EXPERIENCE_ORB;
+            //Spawn an experience orb at the centre point of the plot
+            org.bukkit.entity.Entity tempOrb = Bukkit.getServer().getWorld(e.getEntity().getWorld().getName()).spawnEntity(centrePoint, orb);
+            //Number of entities within the specified plot
+            int pets = tempOrb.getNearbyEntities(modifier, 124, modifier).size();
+            //If too many entities then stop the spawning of any more mobs.
+            //TODO VERY IMPORTANT - Add in check for entities being mobs otherwise experience orbs and carts/boats are included in the count, possibly use a stream to check against all elements in the nearby entities.
+            if (pets >= getConfig().getInt("worlds." + world.getName() + ".allowed-entities")) {
+                Bukkit.getServer().broadcastMessage(Integer.toString(getConfig().getInt("worlds." + world.getName() + ".allowed-entities")));
+                Bukkit.getServer().broadcastMessage("Cancelled due to " + pets);
+                e.setCancelled(true);
+                return;
+            }
         }
-        return;
     }
-
-
 }
 // Test for Git Commit
