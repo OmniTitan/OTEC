a022d3fc3d9917502ccf86b22bb0e35fbeec90fa
OmniTitan <miner.shane@gmail.com>
2015-06-18 20:09:53 +1000

    First useable version. No bugs as of yet. Removed redundent player class. TODO - Add in check to see that plot being spawned in has the owner of the plot in it at the same time as currently players may spawn eggs in others plots, though it still limits them to the configured amount.

--- src/com/omnititan/shaan/OTECPlotMe/Main.java
+++ src/com/omnititan/shaan/OTECPlotMe/Main.java
@@ -1,4 +1,4 @@
-package com.omnititan.shaan.OTEggControl;
+package com.omnititan.shaan.OTECPlotMe;
 
 import com.worldcretornica.plotme_core.PlotMeCoreManager;
 import com.worldcretornica.plotme_core.api.ILocation;
@@ -7,12 +7,16 @@ import com.worldcretornica.plotme_core.bukkit.api.BukkitLocation;
 import com.worldcretornica.plotme_core.bukkit.api.BukkitWorld;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 
 /**
@@ -20,12 +24,50 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class Main extends JavaPlugin implements Listener {
 
+    List<String> entityWatchList = new ArrayList<String>();
+
 
     @Override
     public void onEnable() {
         System.out.println("OT Egg Control Initiated");
+
+        //List of all entities to count when checking for nearby entities.
+        entityWatchList.add("BAT");
+        entityWatchList.add("BLAZE");
+        entityWatchList.add("CAVE_SPIDER");
+        entityWatchList.add("CHICKEN");
+        entityWatchList.add("COW");
+        entityWatchList.add("CREEPER");
+        entityWatchList.add("ENDER_DRAGON");
+        entityWatchList.add("ENDERMITE");
+        entityWatchList.add("GHAST");
+        entityWatchList.add("GIANT");
+        entityWatchList.add("GUARDIAN");
+        entityWatchList.add("HORSE");
+        entityWatchList.add("IRON_GOLEM");
+        entityWatchList.add("MAGMA_CUBE");
+        entityWatchList.add("MINECART");
+        entityWatchList.add("MUSHROOM_COW");
+        entityWatchList.add("OCELOT");
+        entityWatchList.add("PIG");
+        entityWatchList.add("PIG_ZOMBIE");
+        entityWatchList.add("RABBIT");
+        entityWatchList.add("SHEEP");
+        entityWatchList.add("SILVERFISH");
+        entityWatchList.add("SKELETON");
+        entityWatchList.add("SLIME");
+        entityWatchList.add("SNOWMAN");
+        entityWatchList.add("SPIDER");
+        entityWatchList.add("SQUID");
+        entityWatchList.add("VILLAGER");
+        entityWatchList.add("WITCH");
+        entityWatchList.add("WITHER");
+        entityWatchList.add("WOLF");
+        entityWatchList.add("ZOMBIE");
+
         getConfig().options().copyDefaults(true);
         saveConfig();
+
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
 
     }
@@ -37,6 +79,7 @@ public class Main extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onCreatureSpawn(CreatureSpawnEvent e){
+
         //Create plotme IWorld instances for world check
         IWorld world = new BukkitWorld(e.getEntity().getWorld());
         //If in a plotworld then do eggcontrol
@@ -45,7 +88,6 @@ public class Main extends JavaPlugin implements Listener {
             ILocation loc = new BukkitLocation(e.getLocation());
             //Get the plots ID for later use
             String plotID = PlotMeCoreManager.getInstance().getPlotId(loc);
-            Bukkit.getServer().broadcastMessage(plotID);
             //Check if there is actually a plot at this location, getPlotId returns an empty string if nothing is found.
             if (plotID.equals("")) {
                 e.setCancelled(true);
@@ -64,16 +106,26 @@ public class Main extends JavaPlugin implements Listener {
             //Spawn an experience orb at the centre point of the plot
             org.bukkit.entity.Entity tempOrb = Bukkit.getServer().getWorld(e.getEntity().getWorld().getName()).spawnEntity(centrePoint, orb);
             //Number of entities within the specified plot
-            int pets = tempOrb.getNearbyEntities(modifier, 124, modifier).size();
+            List entities = tempOrb.getNearbyEntities(modifier, 124, modifier);
+            tempOrb.remove();
+            //Check to see if entity is a mob that is dissalowed to be over spawned or to be measured against
+            List<Entity> pets = new ArrayList<>();
+            Iterator iterator = entities.iterator();
+            while (iterator.hasNext()) {
+                Entity tempEnt = (Entity) iterator.next();
+                if (entityWatchList.contains(tempEnt.getType().name())) {
+                    pets.add(tempEnt);
+                }
+            }
+
+//            pets.get(1).
             //If too many entities then stop the spawning of any more mobs.
-            //TODO VERY IMPORTANT - Add in check for entities being mobs otherwise experience orbs and carts/boats are included in the count, possibly use a stream to check against all elements in the nearby entities.
-            if (pets >= getConfig().getInt("worlds." + world.getName() + ".allowed-entities")) {
-                Bukkit.getServer().broadcastMessage(Integer.toString(getConfig().getInt("worlds." + world.getName() + ".allowed-entities")));
-                Bukkit.getServer().broadcastMessage("Cancelled due to " + pets);
+
+            if (pets.size() >= getConfig().getInt("worlds." + world.getName() + ".allowed-entities")) {
                 e.setCancelled(true);
                 return;
             }
         }
     }
+
 }
-// Test for Git Commit
